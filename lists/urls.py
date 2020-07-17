9229c7d9f6dcf229172533b11d4db616edac2a85
Bryan Z <piantragrane@protonmail.com>
2020-07-17 15:06:01 -0500

    Refactored list urls2

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'lists'
+urlpatterns = [
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
+]
